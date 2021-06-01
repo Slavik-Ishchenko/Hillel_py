fc1c802d7e49dcf66dc05f454295452873b59e8c
Slavik <dok0630@gmail.com>
2021-06-01 18:13:32 +0300

    Last homework (((

--- Lesson_17/test_for_Les15_Task2.py
+++ Lesson_17/test_for_Les15_Task2.py
@@ -3,9 +3,14 @@ from Lesson_15 import HW15_Task2
 
 
 class PhoneNumberTest(unittest.TestCase):
-    """Должен форматрировать номер телефона по аналогиии: 380639999999 -> (+38) 063 999-99-99"""
+    """Должен форматрировать номер телефона по аналогиии -> (+38) 063 999-99-99"""
     def test_phone_number(self):
-        self.assertEqual(HW15_Task2.format_number('380639999999'), '(+38) 063 999-99-99')
+        self.assertEqual(HW15_Task2.format_number('0639999999'), '(+38) 063 999-99-99')
+        self.assertEqual(HW15_Task2.format_number('063-999-99-99'), '(+38) 063 999-99-99')
+        self.assertEqual(HW15_Task2.format_number('063 999-99-99'), '(+38) 063 999-99-99')
+        self.assertEqual(HW15_Task2.format_number('063-99999-99'), '(+38) 063 999-99-99')
+        self.assertEqual(HW15_Task2.format_number('+3806399-999-99'), '(+38) 063 999-99-99')
+        self.assertEqual(HW15_Task2.format_number('+380639999999'), '(+38) 063 999-99-99')
 
 
 if __name__ == '__main':
